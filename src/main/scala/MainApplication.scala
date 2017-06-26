e368fb6d27a9b75b0bfa4fbb697ea34767dfda8f
zdzarsky <wojtek.zdz@gmail.com>
2017-06-26 10:50:11 +0200

    Two sounds here

--- src/main/scala/MainApplication.scala
+++ src/main/scala/MainApplication.scala
@@ -1,60 +1,97 @@
 import java.io.File
-import javafx.scene.input.KeyCode
 
+import scalafx.scene.input.KeyCode
+import scala.collection.immutable.HashMap
+import scala.collection.mutable.ListBuffer
+import scala.swing.event.ActionEvent
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.control.Button
-import scalafx.scene.input.{KeyEvent, MouseEvent}
-import scalafx.scene.layout.GridPane
+import scalafx.scene.input.{KeyCode, KeyEvent, MouseEvent}
+import scalafx.scene.layout.{GridPane, HBox}
 import scalafx.scene.media._
 import scalafx.scene.paint.Color._
 
+
 object MainApplication extends JFXApp {
+
+  val buttonToMediaPlayerMap = new HashMap[Button, MediaPlayer]
+  val keycodeToButtonMap = new HashMap[KeyCode, Button]
+  val keyList : List[KeyCode] = fillKeyList()
+  val buttonList : ListBuffer[Button] = new ListBuffer[Button]
 
   val BUTTON_PADDING = 4
   val BUTTON_SIZE = 50
   val BUTTON_GRID = 4
 
-  def player_restart(player:MediaPlayer): Unit ={
+  def serveButtonAction(button : Button): Unit ={
+
+  }
+
+  def fillKeyList() : List[KeyCode] ={
+      List(KeyCode.Digit1, KeyCode.Digit2, KeyCode.Digit3, KeyCode.Digit4,
+        KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R,
+        KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.F,
+        KeyCode.Z, KeyCode.X, KeyCode.C, KeyCode.V)
+  }
+
+  def generateGrid(): GridPane ={
+     val grid = new GridPane {
+      margin = Insets(150)
+      hgap = BUTTON_PADDING.asInstanceOf[Double]
+      vgap = BUTTON_PADDING.asInstanceOf[Double]
+     }
+    for (i <- 1 to BUTTON_GRID) {
+      for (j <- 1 to BUTTON_GRID) {
+        val button = new Button{
+
+        }
+        buttonList += button
+        grid.add(button, i, j)
+      }
+    }
+    grid
+  }
+  def choosePane(event : KeyEvent) : Unit = {
+    event.code match {
+      case KeyCode.A => player_restart(mediaPlayer)
+      case KeyCode.B => player_restart(mediaPlayer1)
+      case _ => ;
+    }
+  }
+
+  def player_restart(player: MediaPlayer): Unit = {
     player.stop()
     player.play()
   }
 
+
   val bip: String = "C:\\Users\\Wojciech\\Desktop\\ScalaProject\\ScalaLaunchPad\\src\\main\\resources\\track1.wav"
   val bip1: String = "C:\\Users\\Wojciech\\Desktop\\ScalaProject\\ScalaLaunchPad\\src\\main\\resources\\track2.wav"
+
 
   val hit: Media = new Media(new File(bip).toURI.toString)
   val mediaPlayer: MediaPlayer = new MediaPlayer(hit)
   val mediaPlayer1: MediaPlayer = new MediaPlayer(new Media(new File(bip1).toURI.toString))
 
+
   stage = new PrimaryStage {
-    title = "ScalaFX Hello World"
+    title = "Launchpad"
+    println(keyList.size)
     scene = new Scene {
+      val buttonGrid = generateGrid()
       fill = White
-      val grid : GridPane = new GridPane()
-      grid.setPadding(Insets(5))
-      grid.setHgap(BUTTON_PADDING.asInstanceOf[Double])
-      grid.setVgap(BUTTON_PADDING.asInstanceOf[Double])
-
-      for(i <- 0 to BUTTON_GRID){
-        for(j <- 0 to BUTTON_GRID){
-          val button = new Button("Mah")
-          button.layoutX = BUTTON_SIZE
-          button.layoutX = BUTTON_SIZE
-          grid.add(button, j, i)
-        }
+      content = new HBox {
+        children = Seq(buttonGrid)
       }
 
-      onKeyPressed = (event: KeyEvent) => { event.getCode match {
-          case KeyCode.A => player_restart(mediaPlayer)
-          case KeyCode.B => player_restart(mediaPlayer1)
-        }
-      }
+      onKeyPressed = (event: KeyEvent) => choosePane(event)
 
-      // new Alert(AlertType.Information, "Hello Dialogs!!!").showAndWait()
-      }
+
+    }
   }
+
 }
